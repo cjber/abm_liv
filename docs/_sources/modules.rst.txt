322e0c64134dec8cd37556db47104353fc11073c
cjber <cjberragan@gmail.com>
2019-12-05 13:05:54 +0000

    add docs dir

---
+++ docs/_sources/modules.rst.txt
@@ -0,0 +1,11 @@
+abm_liv
+=======
+
+.. toctree::
+   :maxdepth: 4
+
+   api
+   crime
+   data_clean
+   main
+   police
--- script.sh
+++ script.sh
@@ -1,4 +1,3 @@
 pyreverse api.py police.py crime.py main.py -o png
-
-sphinx-apidoc -o . . 
-rm conf.rst
+make github
+mv *.rst ./source/
